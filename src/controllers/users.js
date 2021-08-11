3974efcb0e2d9e907db925026e6352c9d3d8d0a8
estefaneluz <estefanecortes@gmail.com>
2021-08-11 16:33:22 -0300

    exportações dos controllers de usuários criadas

---
+++ src/controllers/users.js
@@ -0,0 +1,18 @@
+const connection = require("../connection");
+const securePassword = require("secure-password");
+
+const pwd = securePassword();
+
+const registerUser = async (req, res) => {
+
+}
+
+const getUser = async (req, res) => {
+
+}
+
+const editUser = async (req, res) => {
+
+}
+
+module.exports = { registerUser, getUser, editUser }
