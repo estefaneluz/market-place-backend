7f42bd25bfe966f66a80535918100524a3d1a932
estefaneluz <estefanecortes@gmail.com>
2021-08-11 17:07:28 -0300

    add arquivo sql

---
+++ schema.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE market_cubos;
+
+CREATE TABLE IF NOT EXISTS usuarios(
+  id serial PRIMARY KEY,
+  nome varchar(50) NOT NULL,
+  nome_loja varchar(50) NOT NULL,
+  email varchar(100) UNIQUE NOT NULL,
+  senha text NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS produto(
+  id serial PRIMARY KEY,
+  usuario_id INT NOT NULL,
+  nome varchar(100) NOT NULL,
+  estoque int NOT NULL,
+  categoria text,
+  preco int NOT NULL,
+  descricao varchar(100) NOT NULL,
+  imagem text
+);
