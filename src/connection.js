61a3fd9cdcc4c6870d35445a15cf79e602be677a
estefaneluz <estefanecortes@gmail.com>
2021-08-11 16:26:21 -0300

    conexão criada

---
+++ src/connection.js
@@ -0,0 +1,15 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'market_place',
+    password: 'portgres',
+    port: 5432
+})
+
+const query = (text, param) => {
+    return pool.query(text, param);
+}
+
+module.exports = { query }
