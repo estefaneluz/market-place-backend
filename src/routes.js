309fd20f984177ae74e4678b9451903355d8b7ec
estefaneluz <estefanecortes@gmail.com>
2021-08-11 16:38:20 -0300

    rotas de usuarios adicionadas

--- src/routes.js
+++ src/routes.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const users = require("./controllers/users");
+
+const routes = express();
+
+//users
+routes.post("/cadastro", users.registerUser);
+routes.get("/perfil", users.getUser);
+routes.put("/perfil", users.editUser)
+
 module.exports = routes;
